2661ac2d89a814be61158124b54a3fc7858e7ece
Andre <anaki11@outlook.com>
2022-09-08 09:36:12 -0700

    put string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
  * main - Returns value to sdt output
  *
  * Return: 0 if +ve else error
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle \n");
