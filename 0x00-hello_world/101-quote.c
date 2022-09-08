d05cbab7d8f07f03ba845a4ed826381ed795bfd9
Andre <anaki11@outlook.com>
2022-09-08 11:19:37 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - mother of all functions
+ *
+ * fprintf: sending a string to a particular stream
+ *
+ * Return: 0 if successful else 1 if sent to stderr succesfully
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
