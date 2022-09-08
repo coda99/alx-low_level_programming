1de8630d1aaf391c28d2dd499e65c81782cbf606
Andre <anaki11@outlook.com>
2022-09-08 09:52:02 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -mother of all functions
+ *
+ * printf: displays message to std out
+ *
+ * Return: 0 if successful else error message
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
