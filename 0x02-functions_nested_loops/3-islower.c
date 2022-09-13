01d94236f81b66c3a9e348ae9950b8c5a7b52d3d
Andre <anaki11@outlook.com>
2022-09-13 10:53:50 -0700

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,9 @@
 int _islower(int c)
 {
-	int i;
-
-	for (i = 97; i < 123; i++)
-	{
-		if (i == _putchar(c + '0'))
-		{
-			return (1);
-		}
-		else{
-			return (0);
-		}
+	if (c > 'a' && c < 'z'){
+		return (1);
+	}else{
+		return (0);
 	}
 	
 	return (0);
