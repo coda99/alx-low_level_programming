979b2baae97d7c07e291a0bea776102c23e09a82
Andre <anaki11@outlook.com>
2022-09-13 10:42:00 -0700

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
-#include "main.h"
-
-int main(void)
+int _islower(int c)
 {
-	int r; 
+	int i;
 
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+	for (i = 97; i < 123; i++)
+	{
+		if (i == &c)
+		{
+			return (1);
+		}
+		else{
+			return (0);
+		}
+	}
 }
