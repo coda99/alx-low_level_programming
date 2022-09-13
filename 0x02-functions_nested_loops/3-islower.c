231622ff47e1b16b2b44afdca52ef5f9a1d2d25d
Andre <anaki11@outlook.com>
2022-09-13 10:57:37 -0700

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - function to checl for lower chars
+ *
+ * @c: int val for argument
+ *
+ * Return: 0
+ */
+
 int _islower(int c)
 {
-	if (c > 'a' && c < 'z'){
+	if (c > 'a' && c < 'z')
+	{
 		return (1);
-	}else{
-		return (0);
 	}
-	
-	return (0);
+	else
+		return (0);
 }
