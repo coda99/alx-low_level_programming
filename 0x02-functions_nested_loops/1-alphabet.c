4eb8477e7cd5cea37f7d27ace70c1393ffec67f3
Andre <anaki11@outlook.com>
2022-09-13 09:00:35 -0700

    gg

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,17 +3,21 @@
 /**
  * main - check the code
  *
- * Return: Always 0
+ * print_alphabet: sends code to main for printing
+ *
+ * Return: Alwasy 0
  */
 
 void print_alphabet(void)
 {
 	char ch;
-	for (ch = 'a'; ch <= 'z'; ch++ )
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
 	}
-	putchar('\n')
+
+	_putchar('\n');
 }
 
 int main(void)
