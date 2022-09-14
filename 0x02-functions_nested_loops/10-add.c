28887e22940f8f10ebcdf1e2babc1db757a0a57c
Andre <anaki11@outlook.com>
2022-09-14 08:16:00 -0700

    11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two int
+ * @num1: first arg
+ * @num2: 2nd arg
+ *
+ * Return: always 0
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * print_to_98 - Prints all natural nums from n to 98, sep by comma and followed by space.
+ * @n: arg
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("d\n", n);
+	} else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
