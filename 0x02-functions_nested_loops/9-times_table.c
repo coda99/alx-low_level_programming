cc25a36f752f2de5f99330d9c497809969f58ad9
Andre <anaki11@outlook.com>
2022-09-14 07:50:34 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * time_table - sends the mul table to stdout starting from 0
+ */
+
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0');
+
+		for (j = 1; j < 10; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			k = i * j;
+			if (k < 10)
+				_putchar(' ');
+			else
+				_putchar((k / 10) + '0');
+		}
+		
+		_putchar('\n');
+	}
+}
