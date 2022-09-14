b0859677646fc31ec14cc67d80f4e9f7a883f65f
Andre <anaki11@outlook.com>
2022-09-14 07:36:07 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * jack_bauer - func that prints time from 00:00 to 23:59
+ *
+ * Return:  0
+ */
+
+void jack_bauer(void)
+{
+	int hr = 0;
+	int min = 0;
+	int hr_rem;
+	int min_rem;
+
+	while (hr < 24)
+	{
+		while (min < 60)
+		{
+			hr_rem = hr % 10;
+			_putchar(hr / 10 + '0');
+			_putchar(hr_rem + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min_rem + '0');
+			min++;
+			_putchar('\n');
+		}
+		hr++;
+		min = 0;
+	}
+}
