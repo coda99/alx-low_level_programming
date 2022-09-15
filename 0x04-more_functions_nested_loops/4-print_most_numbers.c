891a457c51a2d4130cd61d8ab7c93fbf32e2a77a
Andre <anaki11@outlook.com>
2022-09-15 09:02:14 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_numbers - function to print numbers to stdout
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
