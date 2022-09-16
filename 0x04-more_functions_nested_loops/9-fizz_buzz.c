d3e959e143d645b59eb4d6f8b6ec8dc32c8940b4
Andre <anaki11@outlook.com>
2022-09-16 09:04:52 -0700

    fizz-buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ",i);
+	}
+	putchar('\n');
+	return (0);
+}
