50ea9fbe9928222b6b0e5995b16cce6d43e8b030
Andre <anaki11@outlook.com>
2022-09-19 09:39:02 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swaps values
+ * @a: first swap value
+ * @b: second swap value
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = &b;
+	*b = &a;
+}
