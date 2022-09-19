47a6b5cf0e5bd48ccd5a066a42532155e3acefcc
Andre <anaki11@outlook.com>
2022-09-19 11:43:29 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - string length
+ * @s: args
+ *
+ * Return: 0 always
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i < s; i++)
+	{
+		return (i);
+	}
+}
