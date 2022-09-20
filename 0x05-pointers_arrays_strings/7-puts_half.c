41add5a9bf19aa6bb9dcf55d27f96484812e3aee
Andre <anaki11@outlook.com>
2022-09-20 07:49:28 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,24 +11,26 @@ void puts_half(char *str)
 {
 	int len = strlen(str);
 
-	if (len % 2 != 0)
+	if (len % 2 == 0)
 	{
-		int odd = (len - 1) / 2;
-		while (*(str + odd) != '\0')
+		int half = len / 2;
+
+		while (*(str + half) != '\0')
 		{
-			putchar(*(str + odd));
-			odd++;
+			putchar(*(str + half));
+			half++;
 		}
 		putchar('\n');
 	}
 
 	else
 	{
-		int half = len / 2;
-		while (*(str + half) != '\0')
+		int odd = (len - 1) / 2;
+
+		while (*(str + odd) != '\0')
 		{
-			putchar(*(str + half));
-			half++;
+			putchar(*(str + odd));
+			odd++;
 		}
 		putchar('\n');
 	}
