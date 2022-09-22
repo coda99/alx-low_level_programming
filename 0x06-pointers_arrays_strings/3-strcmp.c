60dd7963a4e3b6abf9d235fd537be41010521187
Andre <anaki11@outlook.com>
2022-09-22 11:07:06 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - concatinates two strings with n bytes
+ *
+ * @src: first string
+ * @dest: second string
+ * @n: third arg
+ *
+ * Return: Dest as final result
+ */
+
+char _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+
+	return (s2);
+}
