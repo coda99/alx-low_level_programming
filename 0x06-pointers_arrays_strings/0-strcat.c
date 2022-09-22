9611981082a32fdd2d14c27654d556d0db7f61de
Andre <anaki11@outlook.com>
2022-09-22 07:19:10 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcat - concatinates two strings
+ *
+ * @src: first string
+ * @dest: second string
+ *
+ * Return: Dest as final result
+ */
+
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
