355edb278dea7ad18814a89389d1daf377aea451
Andre <anaki11@outlook.com>
2022-09-23 06:13:31 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,6 @@
 char *cap_string(char *str)
 {
 	int i = 0;
-	char c = 0;
 
 	while (*(str + i))
 	{
@@ -20,7 +19,7 @@ char *cap_string(char *str)
 		while (!(*(str + i) >= 'a' && *(str + i) <= 'z'))
 			i++;
 
-		if ((*(str + c) >= 32  && *(str + c) <= 46) || *(str + i) == '\t' || *(str + i) == '\n' || i == 0)
+		if (*(str + c) == ','  || *(str + c) == ';' || *(str + i) == '.' || *(str + i) == '!' || *(str + i) == '?' || *(str + i) == '"' || *(str + i) =='(' || *(str + i) == ')' || *(str + i) == '{' || *(str + i) == '}' || *(str + i) == '\t' || *(str + i) == '\n' || i == 0)
 			*(str + i) -= 32;
 		i++;
 	}
