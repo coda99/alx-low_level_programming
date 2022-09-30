537693171cdde6e3b00350c4d13ab0faa1ba1d55
Andre <anaki11@outlook.com>
2022-09-30 10:02:32 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - mother of all funcs
+ * @argc: arg count
+ * @argv: pointer to char arrays
+ *
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) *argv;
+	printf("%d\n", argc);
+	return (0);
+}
