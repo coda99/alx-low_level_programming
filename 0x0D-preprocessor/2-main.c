3499bb0fc402a88f0a550e88d85ba344342563e3
Andre <anaki11@outlook.com>
2022-10-11 05:36:09 -0700

    0x0D

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - mother of all func
+ * @argc: param
+ * @argv: param too
+ *
+ * Return: 0
+ */
+
+int main(int argc, int *argv[])
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTIOM_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
