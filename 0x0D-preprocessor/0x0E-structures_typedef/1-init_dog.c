5b759127cc671b7957e31ae623d897ba0c769340
Andre <anaki11@outlook.com>
2022-10-11 07:07:41 -0700

    0x0D

---
+++ 0x0D-preprocessor/0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Initializes a vrariable of type struct dog
+ * @d: The dog to be initialized
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
