1b6dcb18848152650e2e93c7ff0f30b21ca76596
Andre <anaki11@outlook.com>
2022-10-12 07:15:28 -0700

    0x0F

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calc the sum of two int
+ * @a: first param
+ * @b: second param
+ *
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calc the diff of two int
+ * @a: first param
+ * @b: second param
+ *
+ * Return: diff of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - calc the prod of two int
+ * @a: first param
+ * @b: second param
+ *
+ * Return: prod of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - calc the div of two int
+ * @a: first param
+ * @b: second param
+ *
+ * Return: div of a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calc the mod of two int
+ * @a: first param
+ * @b: second param
+ *
+ * Return: mod of a and b
+ */
+
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
